22ca4050ec591211ec6f18be1bdccb395868e3a2
xiaoruxiang <xiaoruxiang>
2019-09-25 18:33:40 +0800

    全部异步

--- main.lua
+++ main.lua
@@ -1,55 +1,141 @@
-require ("init")
-require ("game")
-require ("MyLib")
-require ("interfaceList")
 
 
-while true do
-	mSleep(2000)
-	print("开始移动------------")
-	movePage(-1,2,teamRange)
-	print("结束------------")
-	mSleep(1000)
+-- ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓全局是否移动标识 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+-- 使用物品
+useGoods = false;
+
+
+-- 移动
+isMove = false;
+oldIndex = "";
+-- 全局orc 识别
+local width,height = getScreenSize()
+sysLog("code = " .. width .. ", height = "..height )
+ocr, msg = createOCR({
+    type = "tesseract", -- 指定tesseract引擎
+    path = "[external]", -- 使用开发助手/叉叉助手的扩展字库
+    lang = "chi_sim" -- 使用英文增强字库(注意需要提前下载好)
+})
+if ocr ~= nil then
+    -- ocr 创建成功，使用该实例进行后续识别操作（参见下面函数文档）
+    sysLog("createOCR succeed: Tesseract-OCR v" .. msg)
+else
+    -- ocr 创建失败，根据msg提示调整
+    sysLog("createOCR failed: " .. tostring(msg))
+end
+
+-- 物品标识列表
+userGoodsList={}
+-- 对话框标识列表
+dialogList={}
+-- 检查移动参数
+indexRange = {
+    {600,3,809,3+34},{"0xffffff-0x000000"},"0123456789APM:"
+}
+
+
+-- ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑全局是否移动标识↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+-- 检查是否移动
+function FIsMove()
+    print("11111111>FIsMove 执行<111111")
+    local code, text = ocr:getText({
+        rect = indexRange[1],
+        diff = indexRange[2], -- 时间颜色为纯黑
+        whitelist = indexRange[3] -- 添加所有可能出现的字符作为白名单
+    })
+    sysLog("code = " .. tostring(code) .. ", text = "..text )
+
+    if code == 0 then
+        if oldIndex == text then
+            -- 没有移动
+            isMove = false
+            task.execTimer(3000, FIsMove)
+            sysLog("没有移动, 三秒后继续..." )
+        else
+            isMove = true
+            oldIndex = text;
+            task.execTimer(1, FIsMove)
+        end
+
+    else
+        sysLog(" 识别失败, 重新刷................" )
+        task.execTimer(1, FIsMove)
+    end
+    sysLog("移动状态: ".. isMove)
+end
+
+-- 检查是否使用物品
+function FUserGood()
+    print("2222222>FUserGood 执行<222222")
+    -- 多点找色 确定是否物品弹窗
+    --local page = F获取指定当前页面(userGoodsList)
+    --if page ~= "未知页面" then
+    if true then
+        sysLog("********有弹窗*************")
+        -- 确认什么物品
+        useGoods = true;
+        task.execTimer(2000, FUserGood)
+    else
+        useGoods = false;
+        task.execTimer(200, FUserGood)
+    end
+end
+
+-- 检查对话框
+function FDialog()
+    print("=>=>=>FDialog 执行<=<=<=<=")
+    -- 遍历弹框列表 确定是否弹窗
+    local page = F获取指定当前页面(closeList)
+    if page ~= "未知页面" then
+        sysLog("********有弹窗*************")
+        -- 确认是什么弹窗
+        useGoods = true;
+        task.execTimer(2000, FUserGood)
+    else
+        useGoods = false;
+        task.execTimer(200, FUserGood)
+    end
+end
+
+-- 战斗页面检查
+function FCombat()
+    print("->->->FCombat 执行<-<-<-<-")
+
+    -- 目前是右下角 单点检查
+    --local page = F多点找色()
+    --if page == "战斗页面" then
+    if true then
+        sysLog("********战斗页面*************")
+        task.execTimer(1000, FCombat)
+    else
+        task.execTimer(200, FCombat)
+    end
 end
 
 
 
+local t = 0
+local function showA()
+    print("++++++showA 执行++++++++")
+    id = createHUD()     --创建一个HUD
+    showHUD(id,"欢迎使用叉叉脚本"..t,12,"0xffff0000","0xffffffff",0,100,0,228,32)      --显示HUD内容
+    mSleep(2000)
+    task.execTimer(2000, showA)
+end
 
 
-
-
-
-
-
-
-
---print("***********初始化函数***********")
---appInit()
---mSleep(1000)
---while true do
---	print("***********登录函数***********")
-----	login()
---	mSleep(1000)
-
---	print("***********开始日常***********")
---	executeTask()
-
---	print("***********切换账号***********")
-----	changeAccount()
---end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while true do
+    -- 异步检查移动
+    task.execTimer(2000, FIsMove)
+    -- 异步检查商品
+    task.execTimer(2000, FUserGood)
+    -- 异步showHUD
+    task.execTimer(2000, showA)
+    mSleep(1000)
+    print("-------while 循环中-------")
+    t = t + 1
+end
 
 
